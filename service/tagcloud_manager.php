cd92ffa4ac54ab1dab411afdf6e36b958097bdf3
S. McCandlish <smccandlish@gmail.com>
2025-03-20 10:51:30 -0700

    Update tagcloud_manager.php
    2nd overhaul.  Extensively rewritten to use existing db-access, security, and sorting code. Finer-grained tag sizing.

--- service/tagcloud_manager.php
+++ service/tagcloud_manager.php
@@ -2,7 +2,7 @@
 /**
 *
 * @package phpBB Extension - RH Topic Tags
-* @copyright © 2014 Robert Heim
+* @copyright © 2014 Robert Heim; significant overhauling and new functions © 2025 S. McCandlish (under same license).
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
 *
 */
@@ -14,6 +14,7 @@ namespace robertheim\topictags\service;
  */
 use robertheim\topictags\tables;
 use robertheim\topictags\prefixes;
+use robertheim\topictags\service\db_helper;
 use robertheim\topictags\service\tags_manager;
 
 /**
@@ -42,6 +43,9 @@ class tagcloud_manager
 	/** @var tags_manager */
 	private $tags_manager;
 
+	/** @var db_helper */
+	private $db_helper;
+
 	public function __construct(
 		\phpbb\db\driver\driver_interface $db,
 		\phpbb\config\config $config,
@@ -49,7 +53,8 @@ class tagcloud_manager
 		\phpbb\user $user,
 		\phpbb\controller\helper $helper,
 		$table_prefix,
-		tags_manager $tags_manager
+		tags_manager $tags_manager,
+		db_helper $db_helper
 	)
 	{
 		$this->db = $db;
@@ -59,12 +64,15 @@ class tagcloud_manager
 		$this->helper = $helper;
 		$this->table_prefix = $table_prefix;
 		$this->tags_manager = $tags_manager;
+		$this->db_helper	= $db_helper;
 	}
 
 	/**
 	 * Assigns all required data for the tag cloud to the template so that
 	 * including tagcloud.html can display the tag cloud.
-	 * @param $maxtags	The maximum number of assigned tags to display. If 0 (default), the config limit is used; if $maxtags <= -1 all tags will be shown; $maxtags otherwise.
+	 * @param $maxtags	The maximum number of assigned tags to display. If 0
+	 *  (default), the config limit is used; if $maxtags <= -1 all tags will
+	 *  be shown; $maxtags otherwise.
 	 */
 	public function assign_tagcloud_to_template($maxtags = 0)
 	{
@@ -76,18 +84,19 @@ class tagcloud_manager
 		// Get the data via get_top_tags(), which abstracts fetching/processing
 		// When $maxtags is still 0, no tags should be displayed.
 		if (0 == $maxtags) {
+		 //This script uses "Yoda order" in `if $0 == $maxtags` to avoid
+		 // silently zeroing the value of the variable, in the event a later
+		 // edit of this code accidentally used `=` in place of `==`.
+		 // Instead of unintended variable assignment happening almost
+		 // undetectably, an error would be thrown.
 			$tags = array();
 		} else {
 			$tags = $this->get_top_tags($maxtags);
-		} //This script uses "Yoda order" in `if $0 == $maxtags` to avoid
-		  // silently zeroing the value of the variable, in the event a later
-		  // edit of this code accidentally used `=` in place of `==`.
-		  // Instead of unintended variable assignment happening almost
-		  // undetectably, an error would be thrown.
+		}
 
-		$maximum = $this->get_maximum_tag_usage_count();
+		$max_count = $this->get_maximum_tag_usage_count($tags);
 
-		$result_size = sizeof($tags);
+		$result_size = count($tags);
 		if ($result_size < $maxtags) {
 			$maxtags = $result_size;
 		}
@@ -107,7 +116,7 @@ class tagcloud_manager
 
 		// Display the tag cloud:
 		foreach ($tags as $tag) {
-			$css_class = $this->get_css_class($tag['count'], $maximum);
+			$css_class = $this->get_css_class($tag['count'], $max_count);
 			$link = $this->helper->route('robertheim_topictags_show_tag_controller', array(
 				'tags'	=> urlencode($tag['tag'])
 			));
@@ -122,89 +131,154 @@ class tagcloud_manager
 	}
 
 	/**
-	 * Gets the most-used tags, up to $maxtags (if positive).
+	 * Gets the most-used tags (from topics the user actually has access to),
+	 * up to $maxtags (if positive).
 	 *
 	 * @param $maxtags	maximum number of results, gets all tags if <1
 	 * @return array	(array('tag' => string, 'count' => int), ...)
 	 */
 	public function get_top_tags($maxtags)
 	{
-		$where = '';
-		if ($maxtags > 0) {
-			$where = 't.count > 0';
-		}
+		// Get the query for permissible topics for the current user
+		$topic_query = $this->tags_manager->get_topics_build_query([], 'AND', false, true);
+		// This is telling that function to return a list of all tagged topics
+		// (`true`) that the user can access, case-insensitively (`false`),
+		// rather than those tagged with a specific tag (`[]` empty array).
+		// The `AND` must be passed just to fill that parameter slot, but has
+		// no impact on functionality in this case.
+
+		// Build the SQL array, appending the topic query directly:
 		$sql_array = array(
-			'SELECT'	=> 't.tag, t.count',
+			'SELECT'	=> 't.tag, t.tag_lowercase, COUNT(tt.tag_id) AS count',
 			'FROM'		=> array(
-				$this->table_prefix . tables::TAGS  => 't'
+				$this->table_prefix . tables::TAGS		=> 't',
+				$this->table_prefix . tables::TOPICTAGS	=> 'tt',
+				'(' . $topic_query . ')' => 'topics'  // Query as derived table
 			),
-			'WHERE'		=> $where,
-			'ORDER_BY'	=> 't.count DESC',
+			'WHERE'		=> 'tt.tag_id = t.id AND tt.topic_id = topics.topic_id',
+			'GROUP_BY'  => 't.id, t.tag, t.tag_lowercase'
 		);
+
+		// Conditionally add a HAVING clause (if applicable) to filter by tag
+		// associations with (accessible) topics, and only include tags with
+		// at least one associated topic:
+		if ($maxtags > 0) {
+			$sql_array['HAVING'] = 'COUNT(tt.tag_id) > 0';
+		}
+
+		$sql_array['ORDER_BY'] = 'count DESC';
+
 		$sql = $this->db->sql_build_query('SELECT', $sql_array);
+
+		// Initialize array we use later:
+		$tags = array();
+
+		// Use the proper query method depending on $maxtags:
 		if ($maxtags > 0) {
 			$result = $this->db->sql_query_limit($sql, (int) $maxtags);
 		} else {
 			$result = $this->db->sql_query($sql);
 		}
-		$tags = array();
+
+		// If query failed or is empty, return early with empty $tags array:
+		if (!$result) {
+			return $tags;
+		}
+
+		// Otherwise, fetch the tags and prepare them for display:
 		while ($row = $this->db->sql_fetchrow($result)) {
 			$tags[] = array(
-				'tag'	=> $row['tag'],
-				'count'	=> $row['count']
+				'tag'			=> $row['tag'],
+				'tag_lowercase'	=> $row['tag_lowercase'],
+				'count'			=> $row['count']
 			);
 		}
-		return $tags;
+
+		// If no tags were found, return early with empty $tags array:
+		if (empty($tags)) {
+			return $tags;
+		}
+
+		// Otherwise, return the (user-accessible) tags, sorted using our
+		// human-friendly algorithm:
+		return $this->tags_manager->sort_tags($tags, false, true);
 	}
 
 	/**
-	 * Get the usage count of the tag that is used the most
+	 * Get the usage count of the tag that is used the most (in topics
+	 * accessible to the user). 
 	 *
-	 * @return int	maximum
+	 * @param array $tags	List of accessible tags with their count data
+	 * @return int			Usage count of the most-used (accessible) tag
 	 */
-	private function get_maximum_tag_usage_count()
+	private function get_maximum_tag_usage_count($tags)
 	{
-		$sql_array = array(
-			'SELECT'	=> 't.count',
-			'FROM'		=> array(
-				$this->table_prefix . tables::TAGS  => 't'
-			),
-			'WHERE'		=> 't.count > 0',
-			'ORDER_BY'	=> 't.count DESC',
-		);
-		$sql = $this->db->sql_build_query('SELECT', $sql_array);
-		$result = $this->db->sql_query_limit($sql, 1);
-		$re = (int) $this->db->sql_fetchfield('count');
-		$this->db->sql_freeresult($result);
-		return $re;
+		// If no tags are available, there's no tag usage to count:
+		if (empty($tags)) {
+			return 0;
+		}
+
+		// Find the highest count among the tags
+		$max_count = 0;
+		foreach ($tags as $tag) {
+			if ($tag['count'] > $max_count) {
+				$max_count = $tag['count'];
+			}
+		}
+
+		return $max_count;
 	}
 
 	/**
 	 * Determines the size of the tag depending on its usage count
 	 *
-	 * @param $count	the count of uses of a tag
-	 * @param $maximum	the usage count of the most-used tag
-	 * @return string	the CSS class name
+	 * @param $count		The count of uses of a tag
+	 * @param $max_count	The usage count of the most-used tag
+	 * @return string		The CSS class name
 	 */
-	private function get_css_class($count, $maximum)
+	private function get_css_class($count, $max_count)
 	{
 		$percent = 50;
-		if (0 < $maximum) {
-			$percent = floor(($count / $maximum) * 100);
+		if (0 < $max_count) {
+			$percent = floor(($count / $max_count) * 100);
 		}
 
 		switch (true) {
-			case $percent < 20:
+			case $percent < 10:
 				return 'rh_topictags_smallest';
+			case $percent < 20:
+				return 'rh_topictags_smaller';
 			case $percent < 40:
 				return 'rh_topictags_small';
 			case $percent < 60:
 				return 'rh_topictags_medium';
 			case $percent < 80:
 				return 'rh_topictags_large';
+			case $percent < 90:
+				return 'rh_topictags_larger';
 			default:
 				return 'rh_topictags_largest';
 		}
 	}
+
+	/**
+	 * Returns only tags (in array) with a use count of 1 or greater
+	 *
+	 * @param $tags		array of tags (with at least column names 'tag' and 'count')
+	 * @return string	array of tags, minus original array members without a positive count
+	 */
+	public function discard_zero_use_tags(array $tags)
+	{
+		// Filter out any tags with a count of 0 before returning the result
+		return array_filter($tags, function($tag) {
+			return $tag['count'] > 0;
+		});
+	} /* This is a utility function that might come in handy later, maybe
+		 for testing or admin purposes, but is not presently employed.
+		 We note that the ACP for this extension includes options to purge
+		 various forms of invalid or unused tags that should have been
+		 auto-removed, suggesting that undesirable tag-related states can
+		 happen, so this function may have use in preventing display
+		 of tags resulting from database-cleanup-related issues. */
 
 }
